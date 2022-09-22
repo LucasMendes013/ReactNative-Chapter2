2dad981fc8c82c5ed5b62a0c055ffb7c58ce40be
LucasMendes013 <104436533+LucasMendes013@users.noreply.github.com>
2022-09-22 09:12:11 -0300

    commit final curso 3

---
+++ gofinances/App.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { Dashboard } from './src/pages/Dashboard';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Dashboard/>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+  },
+});
---
+++ gofinances/src/pages/Dashboard/index.tsx
@@ -0,0 +1,12 @@
+import { Text } from 'react-native'
+import React from 'react'
+import { Container, Title } from './styles'
+
+export function Dashboard() {
+    return (
+      <Container>
+        <Title>Dashboardae</Title>
+      </Container>
+    )
+  }
+
